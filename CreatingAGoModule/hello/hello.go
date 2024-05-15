aa20b45f595dc5b7ab87050cc7ad83db66056cfa
Anno9474 <anno9474@gmail.com>
2024-05-15 13:24:56 +0200

    use the function. it works but the output is still the full map which is expected but weird...

--- CreatingAGoModule/hello/hello.go
+++ CreatingAGoModule/hello/hello.go
@@ -7,14 +7,14 @@ import (
 )
 
 func main() {
-
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Anno")
+	names := []string{"Anno", "Dev", "CRO"}
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	fmt.Println(messages)
 }
